0097cb4a0d2e3a0f9c9f8deba4cb03188f366fcf
Tri Hung <koikoidth12@gmail.com>
2024-09-22 10:29:43 +0700

    custom title primary

--- components/common/title-primary.tsx
+++ components/common/title-primary.tsx
@@ -8,13 +8,27 @@ interface TitlePrimaryProps {
   subTitle?: string;
 }
 
-export default function TitlePrimary({ title, subTitle, icon: Icon }: TitlePrimaryProps) {
+export default function TitlePrimary({
+  title,
+  subTitle,
+  icon: Icon,
+}: TitlePrimaryProps) {
   return (
-    <div className="p-3 rounded-lg text-white dark:bg-[#FFE53B] dark:bg-gradient-to-r dark:from-[#e15904] dark:to-[#bc2d2d] bg-[#333] bg-gradient-to-r from-[#4d433d] to-[#5f6c79] w-fit">
-      <h3 className="flex items-center gap-3">
-        {Icon && <Icon />} {title}
-      </h3>
-      {subTitle && <p className="mt-3 font-thin">{subTitle}</p>}
+    <div className="flex items-center gap-3">
+      {Icon && <Icon color="red" />}
+      <div className="flex flex-col gap-3">
+        <h3
+          style={{
+            background: "linear-gradient(to right, #e15904, #bc2d2d)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          {title}
+        </h3>
+        {subTitle && <p className="font-thin text-red-500">{subTitle}</p>}
+      </div>
     </div>
   );
 }
+// dark:bg-[#FFE53B] dark:bg-gradient-to-r dark:from-[#e15904] dark:to-[#bc2d2d] bg-[#333] bg-gradient-to-r from-[#4d433d] to-[#5f6c79]
