2db84234f4f9c69b8a7f992e4cb44e831e13a4c9
Tri Hung <koikoidth12@gmail.com>
2024-09-23 18:13:48 +0700

    add favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link rel="icon" href="./favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
