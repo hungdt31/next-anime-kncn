42854a75604607824daf02063171074a05718ba9
Tri Hung <koikoidth12@gmail.com>
2024-09-23 11:16:47 +0700

    fix next auth url

--- lib/next-auth-provider.tsx
+++ lib/next-auth-provider.tsx
@@ -4,5 +4,5 @@ import { SessionProvider } from "next-auth/react";
 export default function NextAuthProvider({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 }
