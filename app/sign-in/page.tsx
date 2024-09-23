63dccc09603a23e4fb86801e5d649b8f59efb90f
Tri Hung <koikoidth12@gmail.com>
2024-09-23 10:41:00 +0700

    fix ts error config

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,77 +1,13 @@
-"use client";
-import { signIn } from "next-auth/react";
-import Link from "next/link";
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
-import { useToast } from "@/hooks/use-toast";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { SquareArrowLeft } from "lucide-react";
-import { FaGithub } from "react-icons/fa6";
-import { FaGoogle } from "react-icons/fa6";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { NoticeMessage } from "@/components/common/notice-message";
+import { Suspense } from "react";
+import SignInForm from "@/components/sign-in";
+import { SkeletonCards } from "@/components/loading/skeleton";
 
-const SignInButton = () => {
-  const { toast } = useToast();
-  const search = useSearchParams();
-  const [error, setError] = useState<string | null>(null);
-  const handleSignIn = (provider: string) => {
-    signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT })
-      .then((result) => {
-        if (result?.error) alert("Invalid Credentials!");
-        else
-          toast({
-            title: "Logged In",
-            description: "You have successfully logged in.",
-          });
-      })
-      .catch((err) => {
-        alert(`Error Occured: ${err}`);
-      });
-  };
-  useEffect(() => {
-    if (search.get("error") == "OAuthAccountNotLinked") {
-      setError("Another account already exists with the same e-mail address");
-    } else {
-      setError(null);
-    }
-  }, [search]);
+const SignInPage: React.FC = () => {
   return (
-    <div className="mt-[100px] flex justify-center">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-primary">LOGIN TO NEXT ANIME</CardTitle>
-          <CardDescription>Have a good experience</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex gap-5 flex-wrap mb-5">
-            <Button onClick={() => handleSignIn("github")}>
-              <FaGithub className="mr-3" /> Sign In With GitHub
-            </Button>
-            <Button onClick={() => handleSignIn("google")}>
-              <FaGoogle className="mr-3" /> Sign In With Google
-            </Button>
-          </div>
-          <NoticeMessage message={error} />
-        </CardContent>
-        <CardFooter>
-          <Link href="/">
-            <Button variant={"link"} className="flex items-center gap-3">
-              <SquareArrowLeft size={24} /> Not right now?
-            </Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+    <Suspense fallback={<SkeletonCards />}>
+      <SignInForm />
+    </Suspense>
   );
 };
 
-export default SignInButton;
+export default SignInPage;
