16932a787f301c64563120b8d41308eea257f057
Đoàn Trí Hùng <koikoidth12@gmail.com>
2024-10-15 17:11:08 +0700

    comment feature p2

--- components/discussion/discussion-card.tsx
+++ components/discussion/discussion-card.tsx
@@ -47,13 +47,7 @@ export default function DiscussionCard({
           }}
           className="absolute top-0 z-30 bg-black bg-opacity-50 w-full h-full">
         </div>
-        <div className="absolute top-[40%] left-[8%] z-40 flex gap-3">
-          <Avatar className="rounded-lg">
-            <AvatarImage src={userAvatar} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <p className="font-bold mt-1 text-white">{userName}</p>
-        </div>
+
         {/* <div
           style={{
             backgroundColor: animeColor,
@@ -61,26 +55,36 @@ export default function DiscussionCard({
           }}
           className={`h-[75%] w-[120%] rounded-[20px] skew-x-[19deg] skew-y-[-9deg] absolute top-[45%] left-[-15%] z-10`}
         ></div> */}
-        <div className="absolute bottom-[10%] right-[8%] w-[60%] overflow-hidden z-40 flex flex-col items-end gap-3">
+        <div className="absolute bottom-[5%] w-full overflow-hidden z-40 flex flex-col items-end gap-3 px-3">
+          <div className="flex gap-3 self-start">
+            <Avatar className="rounded-lg">
+              <AvatarImage src={userAvatar} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p className="font-bold mt-1 text-white">{userName}</p>
+          </div>
           <div
             style={{
               backgroundColor: animeColor,
               // boxShadow: 'inset 0 0 10px #000'
             }}
-            className="line-clamp-3 text-sm rounded-lg px-3 py-2 text-white"
+            className="line-clamp-3 text-sm rounded-lg px-3 py-2 text-white max-w-[60%]"
             dangerouslySetInnerHTML={{ __html: comment }}
           />
           <Link href={path.anime(animeId)}>
             <button
               style={{
-                color: animeColor
+                color: animeColor,
+                borderColor: animeColor
               }}
-              className="font-bold italic line-clamp-1 overflow-hidden rounded-lg px-2 py-1 whitespace-nowrap text-ellipsis lg:max-w-[200px] md:max-w-[180px] sm:max-w-[150px] max-w-[120px] bg-gray-100 bg-opacity-15">
+              className="font-bold italic line-clamp-1 overflow-hidden rounded-lg px-2 py-1 whitespace-nowrap text-ellipsis lg:max-w-[200px] md:max-w-[180px] max-w-[150px] border-2">
               {animeName}
             </button>
           </Link>
         </div>
-        <p className="absolute left-[8%] top-[10%] pb-2 text-xs z-40 text-white">
+        <p
+
+          className="absolute left-[8%] top-[10%] pb-2 text-xs z-40 text-white">
           {getCreatedTime(new Date(createdAt))}
         </p>
       </AspectRatio>
